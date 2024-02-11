58750b3edadf26774a2cda9be8773d4c0d274b12
Hazem Magdy <hhazem0010@gmail.com>
2024-02-11 15:34:14 +0200

    fix2 square task

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,12 +3,17 @@
 
 
 class Square():
-    """Square Class"""
+    """Square Class
+
+    Attributes:
+        width (int): width of square
+        height (int): height of square
+    """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """function to initialize a new square instance"""
+        """function to initialize a new square"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -16,7 +21,7 @@ class Square():
         """ Area of the square """
         return self.width * self.width
 
-    def Permiter_Of_My_Square(self):
+    def permiter_of_my_square(self):
         """function to get perimiter of square """
         return (self.width * 2) + (self.height * 2)
 
@@ -30,4 +35,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.Permiter_Of_My_Square())
+    print(s.permiter_of_my_square())
