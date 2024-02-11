7accbb6d7466e436e1e4969271fccf65495f7e53
Hazem Magdy <hhazem0010@gmail.com>
2024-02-11 15:29:06 +0200

    fix square task

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Module represent square"""
-class square():
+
+
+class Square():
     """Square Class"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         """function to initialize a new square instance"""
         for key, value in kwargs.items():
@@ -15,7 +16,7 @@ class square():
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def Permiter_Of_My_Square(self):
         """function to get perimiter of square """
         return (self.width * 2) + (self.height * 2)
 
@@ -23,9 +24,10 @@ class square():
         """function that represent string of square"""
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.Permiter_Of_My_Square())
